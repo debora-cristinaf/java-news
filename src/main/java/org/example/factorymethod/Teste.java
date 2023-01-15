35129306eb29a4d3c791805848342349e8669a83
debora-cristinaf <programacaodcrisf@gmail.com>
2023-01-15 20:01:41 -0300

    feat: factory to collection

--- src/main/java/org/example/factorymethod/Teste.java
+++ src/main/java/org/example/factorymethod/Teste.java
@@ -1,9 +1,6 @@
 package org.example.factorymethod;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 public class Teste {
 
@@ -12,6 +9,7 @@ public class Teste {
 		//Factory Method para Collection.
 		List<String> nomes = List.of("Primeiro nome", "Segundo nome");
 		List.of("Terceiro");
-		Map.of("nome", "joao");
+
+		System.out.print(nomes.get(1));
 	}
 }
